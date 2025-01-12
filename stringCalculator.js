445efbb324848ece010fcef86e208aeddf5c38c4
Parthkumar Shiyani <pshiyani006@gmail.com>
2025-01-12 10:22:29 +0530

    Should return some of two numbers passed.

--- stringCalculator.js
+++ stringCalculator.js
@@ -5,6 +5,10 @@ const stringCalculator = {
     add: (numbers) => {
         if (!numbers) return 0;
 
+        if (numbers.includes(',')) {
+            const numbersArray = numbers.split(',');
+            return parseInt(numbersArray[0]) + parseInt(numbersArray[1]);
+        }
         return parseInt(numbers);
     }
 
--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -9,4 +9,8 @@ describe('stringCalculator', () => {
     test('returns the number itself for a single number', () => {
         expect(stringCalculator.add("1")).toBe(1);
     });
+
+    it('should return some of two numbers', () => {
+        expect(stringCalculator.add('1,2')).toBe(3);
+    });
 });
